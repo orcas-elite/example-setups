3300fe70e8c77499a1e8178615ffb50432f1a624
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2018-06-29 17:31:24 +0200

    Update script to parallelize the build

--- schematic-abcde/istio/build-and-push.sh
+++ schematic-abcde/istio/build-and-push.sh
@@ -1,39 +1,49 @@
 #!/usr/bin/env bash
 # Example script automating the build process of the generated services and push generated docker images to docker hub
 
-CUR_DIR=`pwd`
+function builda () {
+	cd gen-a
+	mvn clean package
+	docker build -t orcaselite/schematic-abcde:gen-a .
+	docker push orcaselite/schematic-abcde:gen-a
+	mvn clean
+}
 
-cd gen-a
-mvn clean package
-docker build -t orcaselite/schematic-abcde:gen-a .
-docker push orcaselite/schematic-abcde:gen-a
-mvn clean
-cd $CUR_DIR
+function buildb () {
+	cd gen-b
+	mvn clean package
+	docker build -t orcaselite/schematic-abcde:gen-b .
+	docker push orcaselite/schematic-abcde:gen-b
+	mvn clean
+}
 
-cd gen-b
-mvn clean package
-docker build -t orcaselite/schematic-abcde:gen-b .
-docker push orcaselite/schematic-abcde:gen-b
-mvn clean
-cd $CUR_DIR
+function buildc () {
+	cd gen-c
+	mvn clean package
+	docker build -t orcaselite/schematic-abcde:gen-c .
+	docker push orcaselite/schematic-abcde:gen-c
+	mvn clean
+}
 
-cd gen-c
-mvn clean package
-docker build -t orcaselite/schematic-abcde:gen-c .
-docker push orcaselite/schematic-abcde:gen-c
-mvn clean
-cd $CUR_DIR
+function buildd () {
+	cd gen-d
+	mvn clean package
+	docker build -t orcaselite/schematic-abcde:gen-d .
+	docker push orcaselite/schematic-abcde:gen-d
+	mvn clean
+}
 
-cd gen-d
-mvn clean package
-docker build -t orcaselite/schematic-abcde:gen-d .
-docker push orcaselite/schematic-abcde:gen-d
-mvn clean
-cd $CUR_DIR
+function builde () {
+	cd gen-e
+	mvn clean package
+	docker build -t orcaselite/schematic-abcde:gen-e .
+	docker push orcaselite/schematic-abcde:gen-e
+	mvn clean
+}
 
-cd gen-e
-mvn clean package
-docker build -t orcaselite/schematic-abcde:gen-e .
-docker push orcaselite/schematic-abcde:gen-e
-mvn clean
-cd $CUR_DIR
+builda &
+buildb &
+buildc &
+buildd &
+builde &
+wait
