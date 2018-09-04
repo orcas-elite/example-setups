05f7daef80d3d7a5bd419442d39bc86d2a09f9dd
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2018-09-04 17:45:53 +0200

    Print response

--- schematic-abcde/workload/locustfile.py
+++ schematic-abcde/workload/locustfile.py
@@ -2,12 +2,18 @@ from locust import HttpLocust, TaskSet, task
 
 class UserBehavior(TaskSet):
     @task(1)
-    def productpage(l):
-	l.client.get("/a1")
+    def a1(self):
+        with self.client.get("/a1") as response:
+            self.log_response(response)
 
     @task(1)
-    def productpage(l):
-	l.client.get("/a2")
+    def a2(self):
+        with self.client.get("/a2") as response:
+            self.log_response(response)
+
+    def log_response(self, response):
+        print('%s, %s, %s, %.12f' % (response.status_code, response.reason, response.request.url, response.elapsed.total_seconds()))
+        # TODO: write to file
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
